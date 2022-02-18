7b0ceef4094ecb4eb24f4ad17225d8d7dee58301
Bruno Silva <bruno.silva@ada.ac.uk>
2022-02-18 21:06:26 +0000

    First Try

---
+++ functions/handler.js
@@ -0,0 +1,118 @@
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({lyrics: `On the First day of Christmas
+My true love sent to me
+A partridge in a pear tree.
+
+On the Second day of Christmas
+My true love sent to me
+Two turtle doves, and
+A partridge in a pear tree.
+
+On the Third day of Christmas
+My true love sent to me
+Three French hens,
+Two turtle doves, and
+A partridge in a pear tree.
+
+On the Fourth day of Christmas
+My true love sent to me
+Four calling birds,
+Three French hens,
+Two turtle doves, and
+A partridge in a pear tree.
+
+On the Fifth day of Christmas
+My true love sent to me
+Five gold rings,
+Four calling birds,
+Three French hens,
+Two turtle doves, and
+A partridge in a pear tree.
+
+On the Sixth day of Christmas
+My true love sent to me
+Six geese a-laying,
+Five gold rings,
+Four calling birds,
+Three French hens,
+Two turtle doves, and
+A partridge in a pear tree.
+
+On the Seventh day of Christmas
+My true love sent to me
+Seven swans a-swimming,
+Six geese a-laying,
+Five gold rings,
+Four calling birds,
+Three French hens,
+Two turtle doves, and
+A partridge in a pear tree.
+
+On the Eighth day of Christmas
+My true love sent to me
+Eight maids a-milking,
+Seven swans a-swimming,
+Six geese a-laying,
+Five gold rings,
+Four calling birds,
+Three French hens,
+Two turtle doves, and
+A partridge in a pear tree.
+
+On the Ninth day of Christmas
+My true love sent to me
+Nine ladies dancing,
+Eight maids a-milking,
+Seven swans a-swimming,
+Six geese a-laying,
+Five gold rings,
+Four calling birds,
+Three French hens,
+Two turtle doves, and
+A partridge in a pear tree.
+
+On the Tenth day of Christmas
+My true love sent to me
+Ten lords a-leaping,
+Nine ladies dancing,
+Eight maids a-milking,
+Seven swans a-swimming,
+Six geese a-laying,
+Five gold rings,
+Four calling birds,
+Three French hens,
+Two turtle doves, and
+A partridge in a pear tree.
+
+On the Eleventh day of Christmas
+My true love sent to me
+Eleven pipers piping,
+Ten lords a-leaping,
+Nine ladies dancing,
+Eight maids a-milking,
+Seven swans a-swimming,
+Six geese a-laying,
+Five gold rings,
+Four calling birds,
+Three French hens,
+Two turtle doves, and
+A partridge in a pear tree.
+
+On the Twelfth day of Christmas
+My true love sent to me
+Twelve drummers drumming,
+Eleven pipers piping,
+Ten lords a-leaping,
+Nine ladies dancing,
+Eight maids a-milking,
+Seven swans a-swimming,
+Six geese a-laying,
+Five gold rings,
+Four calling birds,
+Three French hens,
+Two turtle doves, and
+A partridge in a pear tree.`})
+  }
+}
